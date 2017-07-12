db27caf4d9cfe7e9d4e144698213a58ec391f1b7
Robert Schroeder <rob.schroeder@graphicpkg.com>
2017-07-12 16:23:41 -0400

    12 July

--- fix-pauls-data.R
+++ fix-pauls-data.R
@@ -73,3 +73,72 @@ pd <- pd %>%
   filter(!(SAP.Code %in% c("PLT00508", "PLT00019")))
 
 # If the ship to is a Europe port, then use the sold-to to get the actual
+# location.  Rename the SAP.Code field so it doesn't conflict with what we 
+# already have
+pd <-
+  left_join(pd, rename(select(plant_codes, Cust, SAP.Code),
+                               SAP.Sold.To = SAP.Code),
+            by = c("Sold.to.pt" = "Cust"))
+
+# Where are they different
+pd %>% filter(SAP.Code != SAP.Sold.To) %>%
+  group_by(Ship.to, SAP.Code, Sold.to.pt, SAP.Sold.To) %>%
+  summarize(Tons = sum(Tons))
+
+# Update the SAP.Code to the sold where they are not equal (and where the
+# SAP.Sold.To is not NA).  This fixes a
+# Portland issue, too.
+pd <- pd %>%
+  mutate(SAP.Code = ifelse(!is.na(SAP.Sold.To) & (SAP.Code != SAP.Sold.To), 
+                           SAP.Sold.To, SAP.Code))
+
+# Get rid of the Sold.To
+pd <- pd %>%
+  select(-SAP.Sold.To)
+
+# If we don't have an SAP.Code (Plant), then it should be open market
+# or Japan and we don't want it
+pd <- pd %>%
+  ungroup() %>%
+  filter(!is.na(SAP.Code)) %>%
+  rename(Plant = SAP.Code)
+
+# Check the total tons left
+pd %>% summarize(sum(Tons))
+
+# Figure out what to do with the missing machines (2015 data)
+pd <- pd %>%
+  mutate(Machine = ifelse(
+    is.na(Machine),
+    ifelse(
+      (Mill == "0031" | Mill == "31"),            # WM
+      ifelse(Cal >= 24, "06", "07"),
+      ifelse(Cal >= 18, "02", "01")  # Macon
+    ),
+    Machine
+  ))
+
+# Build the Mill-Machine field
+pd <- pd %>% 
+  mutate(Mill = ifelse((Mill == "0031" | Mill == "31"), "W", "M")) %>%
+  mutate(Mill = paste(Mill, as.numeric(Machine), sep = "")) %>%
+  select(-Machine)
+
+# Get rid of the following:
+pd <- pd %>%
+  select(-Ship.to, -Sold.to.pt, -Description, -Int.Ext.Whs, -Grade.Type)
+
+# Summarize again over the fields that are left
+pd <- pd %>%
+  group_by_(.dots = setdiff(names(pd), "Tons")) %>%
+  summarize(Tons = sum(Tons)) %>%
+  ungroup()
+
+# How many SKUs are we starting with
+pd %>% 
+  summarize(Prod.Count = n_distinct(Mill, Grade, Cal, Wind, Width, Dia),
+            Plants = n_distinct(Plant))
+
+# Save as a csv at this point - prior to diameter consolidation
+write_csv(pd, 
+          paste0("Data/All Shipments No SKU Fix ", mYear, ".csv"))
